d9a7da8ad7894f95da4e3a4f10c40622f3a341f8
arthurvergacas <arthurvdm@gmail.com>
2021-04-02 17:48:46 -0300

    Remove body parser library (deprecated)

--- whatsapp-integration/src/index.js
+++ whatsapp-integration/src/index.js
@@ -1,42 +1,39 @@
 // external packages
-const express = require('express');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 
 // Start the webapp
 const webApp = express();
 
 // Webapp settings
-webApp.use(bodyParser.urlencoded({
-    extended: true
-}));
-webApp.use(bodyParser.json());
+
+// Body Parser is deprecated. If we want to parse json, we can use express.json()
+webApp.use(express.json());
+app.use(express.urlencoded());
 
 // Server Port
 const PORT = process.env.PORT;
 
 // Home route
-webApp.get('/', (req, res) => {
-    res.send(`Hello World.!`);
+webApp.get("/", (req, res) => {
+  res.send(`Hello World.!`);
 });
 
-const WA = require('../helper-function/whatsapp-send-message');
+const WA = require("../helper-functions/whatsapp-functions");
 
 // Route for WhatsApp
-webApp.post('/whatsapp', async (req, res) => {
+webApp.post("/whatsapp", async (req, res) => {
+  let message = req.body.Body;
+  let senderID = req.body.From;
 
-    let message = req.body.Body;
-    let senderID = req.body.From;
+  console.log(message);
+  console.log(senderID);
 
-    console.log(message);
-    console.log(senderID);
-
-    // Write a function to send message back to WhatsApp
-    await WA.sendMessage('Bem vindo ao sistema de vendas.', senderID);
-
+  // Write a function to send message back to WhatsApp
+  await WA.sendMessage("Bem vindo ao sistema de vendas.", senderID);
 });
 
 // Start the server
 webApp.listen(PORT, () => {
-    console.log(`Server is up and running at ${PORT}`);
+  console.log(`Server is up and running at ${PORT}`);
 });
