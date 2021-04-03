03ce962c002e640353b22442aa12a17f937f5d69
arthurvergacas <arthurvdm@gmail.com>
2021-04-03 13:09:19 -0300

    Bugfix 🐛: Uncommenting section of code

--- whatsapp-integration/src/helper-classes/Bot.js
+++ whatsapp-integration/src/helper-classes/Bot.js
@@ -3,9 +3,9 @@ const fs = require("fs");
 const accountSid = process.env.TWILIO_ACCOUNT_SID; // Your Account SID from www.twilio.com/console
 const authToken = process.env.TWILIO_AUTH_TOKEN; // Your Auth Token from www.twilio.com/console
 
-// const client = require("twilio")(accountSid, authToken, {
-//   lazyLoading: true,
-// });
+const client = require("twilio")(accountSid, authToken, {
+  lazyLoading: true,
+});
 
 class Bot {
   constructor(firstInteractionName) {
